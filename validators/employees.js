9ada3166d89cde523c0a2d9bd91cb6f56a9b9fc6
Azhi99 <azhijabar.aj@gmail.com>
2020-09-11 17:52:55 +0300

    commit

--- validators/employees.js
+++ validators/employees.js
@@ -3,7 +3,17 @@ const validate = require("./validate.js");
 
 module.exports = {
     createValidation: [
-
+        body("first_name")
+            .isLength({ min: 1 }).withMessage("Enter first name"),  
+        body("last_name")
+            .isLength({ min: 1 }).withMessage("Enter first name"), 
+        body("st_id")
+            .isInt({ min: 1 }).withMessage("Staff ID must be number and greater than 0"), 
+        body("phone")
+            .isLength({ min: 11, max: 11 }).withMessage("Phone number must be 11 digit"), 
+        body("salary_type")
+            .isIn(["Monthly", "Daily"]).withMessage("Salary type must be monthly or daily"), 
+        validate
     ],
     updateValidation: [
         param("emp_id")
