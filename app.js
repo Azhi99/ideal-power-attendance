6beb3f42182253c2685d38beafded538a08c1b71
Tishko serwan <tishko.serwan@gmail.com>
2020-09-11 14:47:30 +0300

    commit

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ const cors = require("cors");
 const jobRouter = require("./routes/job.js");
 const staffRouter = require("./routes/staff.js");
 const userRouter = require("./routes/user.js");
-<<<<<<< HEAD
 const enginnerRouter= require('./routes/engineer.js')
-
-=======
 const employeeRouter = require("./routes/employees.js");
->>>>>>> 352a2456cdff1cbfeccba9679ce294d729120863
 
 const app = express();
 var port = process.env.PORT;
