2ab82eeb9f16a1e3af9aadc1936a8cad7bda8bf7
Azhi99 <azhijabar.aj@gmail.com>
2020-09-11 21:30:33 +0300

    commit

--- routes/attendance.js
+++ routes/attendance.js
@@ -4,7 +4,52 @@ const db = require("../DB/mainDBconfig.js");
 const router = express.Router();
 
 router.post("/addAttendance", (req, res) => {
-    
+    db("tbl_attendance").insert({
+        dsl_id: req.body.dsl_id,
+        emp_id: req.body.emp_id,
+        over_time: 0,
+        worked_hours: 8,
+        fine: 0,
+        fine_reason: req.body.fine_reason,
+        absent: "1"
+    }).then(([data]) => {
+        return res.status(200).json({
+            message: "Attendance Added",
+            at_id: data
+        });
+    }).catch((err) => {
+        return res.status(500).json({
+            message: err
+        });
+    });
+});
+
+router.patch("/setAbsent/:at_id", (req, res) => {
+    db("tbl_attendace").where("at_id", req.params.at_id).update({
+        absent: "0"
+    }).then(() => {
+        return res.status(200).json({
+            message: "Absented"
+        });
+    }).catch((err) => {
+        return res.status(500).json({
+            message: err
+        });
+    });
+});
+
+router.patch("/cancelAbsent/:at_id", (req, res) => {
+    db("tbl_attendace").where("at_id", req.params.at_id).update({
+        absent: "1"
+    }).then(() => {
+        return res.status(200).json({
+            message: "Absent canceled"
+        });
+    }).catch((err) => {
+        return res.status(500).json({
+            message: err
+        });
+    });
 });
 
 module.exports = router;
