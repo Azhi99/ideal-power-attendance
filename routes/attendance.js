fe88da0f1add110f86e851076781b8a27b6f6a52
Azhi99 <azhijabar.aj@gmail.com>
2020-09-11 20:30:21 +0300

    commit

---
+++ routes/attendance.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const db = require("../DB/mainDBconfig.js");
+
+const router = express.Router();
+
+router.post("/addAttendance", (req, res) => {
+    
+});
+
+module.exports = router;
