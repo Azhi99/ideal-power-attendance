b928e47564cb6e6ba9591f4d6e08f93f4ee0b5df
Tishko serwan <tishko.serwan@gmail.com>
2020-09-11 21:23:59 +0300

    commit

--- routes/attendance.js
+++ routes/attendance.js
@@ -7,4 +7,22 @@ router.post("/addAttendance", (req, res) => {
     
 });
 
+
+router.patch('/updateAttendance/:at_id',(req,res)=>{
+    db('tbl_attendance').where('at_id',req.params.at_id).update({
+        overtime:req.body.overtime,
+        worked_hours:req.body.worked_hours,
+        fine:req.body.fine,
+        fine_reason:req.body.fine_reason
+    }).then(()=>{
+        return res.status(200).json({
+            message:'Update'
+        })
+    }).catch((err)=>{
+        return res.status(500).json({
+            message:err
+        })
+    })
+})
+
 module.exports = router;
