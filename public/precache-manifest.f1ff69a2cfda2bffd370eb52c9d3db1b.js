cff1bc0836fd99cbaaf355ed9b0745ab2791ba55
Azhi99 <azhijabar.aj@gmail.com>
2020-09-27 11:09:58 +0300

    commit

---
+++ public/precache-manifest.f1ff69a2cfda2bffd370eb52c9d3db1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa6db0947757df4cf96",
+    "url": "/css/app.b2fceadf.css"
+  },
+  {
+    "revision": "6bde11d7879fc424a4b4",
+    "url": "/css/chunk-vendors.71a233fb.css"
+  },
+  {
+    "revision": "dc9a8bb1a3a65504c8d90566a26d5327",
+    "url": "/fonts/NRT-Reg.dc9a8bb1.ttf"
+  },
+  {
+    "revision": "34439dcccdb0004336016f41b50612f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfa6db0947757df4cf96",
+    "url": "/js/app.c9bb399b.js"
+  },
+  {
+    "revision": "6bde11d7879fc424a4b4",
+    "url": "/js/chunk-vendors.89ad7e08.js"
+  },
+  {
+    "revision": "fdcbedf4fcf82b1666fbcaab5586fb22",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
