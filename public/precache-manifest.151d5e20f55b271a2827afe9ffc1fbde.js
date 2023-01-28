66f3ce929db2a96c7badc38db69eda9d347012b0
Azhi99 <azhijabar.aj@gmail.com>
2023-01-28 21:44:03 +0300

    0107

---
+++ public/precache-manifest.151d5e20f55b271a2827afe9ffc1fbde.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941e875a338bebc3ac4e",
+    "url": "/css/app.15956290.css"
+  },
+  {
+    "revision": "4bafd636804289cec151",
+    "url": "/css/chunk-vendors.89cf69e6.css"
+  },
+  {
+    "revision": "dc9a8bb1a3a65504c8d90566a26d5327",
+    "url": "/fonts/NRT-Reg.dc9a8bb1.ttf"
+  },
+  {
+    "revision": "9f1406b965f5e3bc69d8d7cb9292123b",
+    "url": "/img/Ideal power logo.9f1406b9.png"
+  },
+  {
+    "revision": "cf01e118ab7e6921ed3e27d2cd0e3a41",
+    "url": "/img/logo.cf01e118.png"
+  },
+  {
+    "revision": "3b6cf69acd88aa89569e20c0e215a958",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941e875a338bebc3ac4e",
+    "url": "/js/app.0929e7d1.js"
+  },
+  {
+    "revision": "4bafd636804289cec151",
+    "url": "/js/chunk-vendors.33dfb7bc.js"
+  },
+  {
+    "revision": "fdcbedf4fcf82b1666fbcaab5586fb22",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
