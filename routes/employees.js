1d8b46b1dfc90b2e2ef251392f5ff21d7276f63e
Azhi99 <azhijabar.aj@gmail.com>
2023-08-05 15:19:39 +0300

    fix bug in total salary

--- routes/employees.js
+++ routes/employees.js
@@ -1076,7 +1076,7 @@ router.post('/getSalaryListByMonthAndYearForTotal', async (req, res) => {
         employee_final_with_give_salary.total_accomodation,
         employee_final_with_give_salary.loan_by_accomodation,
         employee_final_with_give_salary.accomodation_by_accomodation,
-        employee_final_with_give_salary.exense_by_accomodation,
+        employee_final_with_give_salary.expense_by_accomodation,
         employee_final_with_give_salary.total_f,
         employee_final_with_give_salary.total_h_not_work,
         ((employee_final_with_give_salary.total_o - employee_final_with_give_salary.total_h_not_work) + employee_final_with_give_salary.added_overtime) as total_hour,
