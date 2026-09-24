e5b94189635ceb198e889c59834ea4bb8813f0c1
Azhi99 <azhijabar.aj@gmail.com>
2026-09-24 14:45:12 +0300

    edit

--- DB/mainDBconfig.js
+++ DB/mainDBconfig.js
@@ -3,7 +3,7 @@ const db = require("knex")({
     client: "mysql",
     connection: {
         host: process.env.HOST,
-        user: process.env.USER,
+        user: process.env.DB_USER,
         password: process.env.PASS,
         database: process.env.MAIN_DB,
         timezone: 'utc'
