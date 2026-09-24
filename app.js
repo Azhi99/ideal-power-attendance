fe928c62bcf5c24892df6e4c757e9dea5b715bc7
Azhi99 <azhijabar.aj@gmail.com>
2026-09-24 15:30:49 +0300

    edit cors

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(cors({
-  origin: ['http://localhost:8080'],
+  origin: '*',
   credentials: true
 }));
 
